673040345b775f2cb6ea077cbb11e48482ecf387
Yohann Payrot <yohann.payrot@gmail.com>
2014-12-14 19:47:55 +0800

    WINOT-71

---
+++ src/Wineot/SearchBundle/Tests/Controller/SearchControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Wineot\SearchBundle\Tests\Controller;
+use Wineot\SearchBundle\Form\Type\SearchType;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class SearchControllerTest extends TypeTestCase
+{
+    public function testSearch()
+    {
+        $formData = array('searchInput' => 'bonjour');
+
+        $type = new SearchType();
+        $form = $this->factory->create($type);
+
+        $form->submit($formData);
+        $this->assertTrue($form->isSynchronized());
+        $view = $form->createView();
+        $children = $view->children;
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
