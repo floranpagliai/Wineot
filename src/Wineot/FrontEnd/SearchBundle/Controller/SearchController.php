4243fda133d07f5be6fc11d2fe4bb2227e0b1480
Floran Pagliai <floran.pagliai@gmail.com>
2015-02-24 20:05:23 +0800

    WINOT-83

--- src/Wineot/FrontEnd/SearchBundle/Controller/SearchController.php
+++ src/Wineot/FrontEnd/SearchBundle/Controller/SearchController.php
@@ -4,10 +4,6 @@ namespace Wineot\FrontEnd\SearchBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpFoundation\JsonResponse;
-
-use Doctrine\ORM\EntityRepository;
 use Wineot\FrontEnd\SearchBundle\Form\Type\SearchType;
 
 
@@ -15,27 +11,36 @@ class SearchController extends Controller
 {
     public function searchAction(Request $request)
     {
-        $form = $this->createForm(new SearchType());
-
-        if ($request->isMethod('POST')) {
-            $form->submit($request);
-            if ($form->isValid()) {
-                $searchInput = $form->getData()["searchInput"];
-                $wineList = $this
-                    ->getDoctrine()
-                    ->getManager()
-                    ->getRepository('WineotDataBundle:Wine')
-                    ->findBy(array('name' => $searchInput));
-                if(!empty($wineList)){
-                    // IL FAUT REDIRECT VERS UNE ACTION QUI VA RENDER A SON TOUR
-                    return $this->render('WineotFrontEndSearchBundle:Search:SearchResult.html.twig', array('wineList' => $wineList));
-                }
-                else{
-                    // AUCUN RESULTATS
-                }
-            }
-        }
+        $form = $this->createForm(new SearchType(), null, [
+            'action' => $this->generateUrl('wineot_search_result'),
+            'method' => 'POST']);
 
         return $this->render('WineotFrontEndSearchBundle:Search:Search.html.twig', array('form' => $form->createView()));
+    }
+
+    public function searchResultAction(Request $request)
+    {
+        $form = $this->createForm(new SearchType());
+
+        $form->submit($request);
+        if ($form->isValid()) {
+            $searchInput = $form->getData()["searchInput"];
+            $wineList = $this
+                ->getDoctrine()
+                ->getManager()
+                ->getRepository('WineotDataBundle:Wine')
+                ->createQueryBuilder('q')
+                ->where('q.name LIKE :searchInput')
+                ->setParameter('searchInput', '%'.$searchInput.'%')
+                ->getQuery()
+                ->getResult();
+            if(!empty($wineList)){
+                return $this->render('WineotFrontEndSearchBundle:Search:SearchResult.html.twig', array('wineList' => $wineList));
+            }
+            else{
+
+                // AUCUN RESULTATS
+            }
+        }
     }
 }
