7b7c2cf938ae9c3da638b11ff7d00488c77d6789
Floran Pagliai <floran.pagliai@gmail.com>
2015-10-23 23:59:13 +0200

    User hotfix

--- src/Wineot/UserBundle/Controller/UserController.php
+++ src/Wineot/UserBundle/Controller/UserController.php
@@ -19,9 +19,10 @@ class UserController extends Controller
 {
     public function profileAction()
     {
-        if (!$this->get('security.context')->isGranted('ROLE_USER'))
-            return $this->redirect($this->generateUrl('wineot_user_login'));
         $user = $this->getUser();
+        if (!$user) {
+            return $this->redirect($this->generateUrl('wineot_user_login'));
+        }
         $favoritesWines = $user->getFavoritesWines();
         $comments = $user->getComments();
         $paramsRender = array(
@@ -32,14 +33,14 @@ class UserController extends Controller
 
     public function editProfileAction(Request $request)
     {
-        if (!$this->get('security.context')->isGranted('ROLE_USER'))
+        $user = $this->getUser();
+        if (!$user) {
             return $this->redirect($this->generateUrl('wineot_user_login'));
+        }
 
         $em = $this->get('doctrine_mongodb')->getManager();
 
-        $user = $this->getUser();
         $form = $this->createForm(new UserEditType(), $user);
-
         $form->handleRequest($request);
             if ($form->isValid()) {
                 $em->persist($user);
