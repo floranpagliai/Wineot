f405252ee65f0a8ae8e70e4246b0a78154128234
Floran Pagliai <floran.pagliai@gmail.com>
2015-10-29 19:46:06 +0100

    Modification transactionnelle de la landing page

--- src/Wineot/FrontEnd/LandingBundle/Controller/DefaultController.php
+++ src/Wineot/FrontEnd/LandingBundle/Controller/DefaultController.php
@@ -10,13 +10,31 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         $mc = $this->get('hype_mailchimp');
+        $mailjet = $this->container->get('headoo_mailjet_wrapper');
+        $flash = $this->get('notify_messenger.flash');
+
         $form = $this->createFormBuilder()
             ->add('email', 'email')
             ->getForm();
         $form->handleRequest($request);
         if ($form->isValid()) {
             $email = $form->getData()['email'];
-            $mc->getList()->subscribe($email);
+//            $mc->getList()->subscribe($email);
+            $params = array(
+                "method" => "POST",
+                "from" => "floran@wineot.net",
+                "to" => $email,
+                "subject" => "Que penses tu de Wine'ot",
+                "html" => $this->renderView('Emails/welcome.html.twig')
+            );
+            $mailjet->sendEmail($params);
+
+            $params = array(
+                "method" => "POST",
+                "Email" => $email
+            );
+            $mailjet->contact($params);
+            $flash->success($this->get('translator')->trans('landing.warn.success'));
             return $this->redirect($this->generateUrl('wineot_front_end_landing_homepage'));
         }
 
