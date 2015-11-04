d38b16dc0b8ca73c246cd0b49d3ea985e648e365
Shked0wn <floran.pagliai@gmail.com>
2015-11-04 14:34:48 +0100

    Creating menu rendering function

--- src/Wineot/BackEnd/CRUDBundle/Controller/DefaultController.php
+++ src/Wineot/BackEnd/CRUDBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Wineot\BackEnd\CRUDBundle\Controller;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
@@ -11,5 +12,18 @@ class DefaultController extends Controller
         if (!$this->get('security.context')->isGranted('ROLE_ADMIN'))
             return $this->redirect($this->generateUrl('wineot_front_end_homepage'));
         return $this->render('WineotBackEndCRUDBundle:Default:index.html.twig');
+    }
+
+    public function renderMenuAction()
+    {
+        $crudMenu = new ArrayCollection();
+
+        $crudMenu->add(array(
+            'route' => 'wineot_back_end_crud_winery',
+            'name' => 'crud.title.wineries',
+            'routes' => array('wineot_back_end_crud_winery', 'wineot_back_end_crud_winery_add', 'wineot_back_end_crud_winery_edit')
+        ));
+        $paramsRender = array('menu' => $crudMenu);
+        return $this->render('WineotBackEndCRUDBundle:Default:index.html.twig', $paramsRender);
     }
 }
