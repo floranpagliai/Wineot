60b75874046516e3eae07d5af3541cec89ca805f
Shked0wn <floran.pagliai@gmail.com>
2015-12-16 16:52:43 +0100

    Refactoring de la recherche pour garder le texte dans le formulaire

--- src/Wineot/FrontEnd/SearchBundle/Form/Type/SearchType.php
+++ src/Wineot/FrontEnd/SearchBundle/Form/Type/SearchType.php
@@ -4,17 +4,21 @@ namespace Wineot\FrontEnd\SearchBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class SearchType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('searchInput', 'text');
+        $builder->add('searchInput', 'text', array(
+            'attr' => array(
+                'placeholder' => 'wine.action.search',
+                'class' => 'search-field form-control input-lg'
+            )
+        ));
     }
 
     public function getName()
     {
-       return 'search';
+        return 'search';
     }
 }
